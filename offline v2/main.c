ce19e74f0574ca4375374ec7dc74ebc56f6a1f22
Grant Kennell <grant.kennell@citrix.com>
2017-09-13 12:15:16 -0400

    offline ex: made init_kernel and create_program_from_bitcode exactly match 2048 ones

--- offline v2/main.c
+++ offline v2/main.c
@@ -113,79 +113,79 @@ static void shutdown_opencl();
 
 static void init_kernel(char* bitcode_path,char* function_name)
 {
-    if (kernel_initialied)
-    {
-        return;
-    }
+  if (kernel_initialied)
+  {
+    return;
+  }
 
-    // Perform typical OpenCL setup in order to obtain a context and command
-    // queue.
-    cl_int err;
-    cl_uint num_devices;
+  // Perform typical OpenCL setup in order to obtain a context and command
+  // queue.
+  cl_int err;
+  cl_uint num_devices;
 
-    // How many devices of the type requested are in the system?
-    clGetDeviceIDs(NULL, device_type, 0, NULL, &num_devices);
+  // How many devices of the type requested are in the system?
+  clGetDeviceIDs(NULL, device_type, 0, NULL, &num_devices);
 
-    // Make sure the requested index is within bounds.  Otherwise, correct it.
-    if (device_index < 0 || device_index > num_devices - 1) {
-        fprintf(stdout, "Requsted index (%d) is out of range.  Using 0.\n",
-                device_index);
-        device_index = 0;
-    }
+  // Make sure the requested index is within bounds.  Otherwise, correct it.
+  if (device_index < 0 || device_index > num_devices - 1) {
+    fprintf(stdout, "Requsted index (%d) is out of range.  Using 0.\n",
+            device_index);
+    device_index = 0;
+  }
 
-    // Grab the requested device.
-    cl_device_id all_devices[num_devices];
-    clGetDeviceIDs(NULL, device_type, num_devices, all_devices, NULL);
-    device = all_devices[device_index];
+  // Grab the requested device.
+  cl_device_id all_devices[num_devices];
+  clGetDeviceIDs(NULL, device_type, num_devices, all_devices, NULL);
+  device = all_devices[device_index];
 
-    // Dump the device.
-    char name[128];
-    clGetDeviceInfo(device, CL_DEVICE_NAME, 128*sizeof(char), name, NULL);
+  // Dump the device.
+  char name[128];
+  clGetDeviceInfo(device, CL_DEVICE_NAME, 128*sizeof(char), name, NULL);
 
-    cl_ulong max_buffer_size;
-    clGetDeviceInfo(device, CL_DEVICE_MAX_PARAMETER_SIZE, sizeof(cl_ulong), &max_buffer_size, NULL);
+  cl_ulong max_buffer_size;
+  clGetDeviceInfo(device, CL_DEVICE_MAX_PARAMETER_SIZE, sizeof(cl_ulong), &max_buffer_size, NULL);
 
-    fprintf(stdout,"CL_DEVICE_MAX_PARAMETER_SIZE: %lld\n",max_buffer_size);
+  fprintf(stdout,"CL_DEVICE_MAX_PARAMETER_SIZE: %lld\n",max_buffer_size);
 
-    fprintf(stdout, "Using OpenCL device: %s\n", name);
+  fprintf(stdout, "Using OpenCL device: %s\n", name);
 
-    // Create an OpenCL context using this compute device.
-    context = clCreateContext(NULL, 1, &device, NULL, NULL, &err);
-    check_status("clCreateContext", err);
+  // Create an OpenCL context using this compute device.
+  context = clCreateContext(NULL, 1, &device, NULL, NULL, &err);
+  check_status("clCreateContext", err);
 
-    // Create a command queue on this device, since we want to use if for
-    // running our CL program.
-    queue = clCreateCommandQueue(context, device, 0, &err);
-    check_status("clCreateCommandQueue", err);
+  // Create a command queue on this device, since we want to use if for
+  // running our CL program.
+  queue = clCreateCommandQueue(context, device, 0, &err);
+  check_status("clCreateCommandQueue", err);
 
-    // Instead of passing actual executable bits, we pass a path to the
-    // already-compiled bitcode to clCreateProgramWithBinary.  Note that
-    // you may load bitcode for multiple devices in one call by passing
-    // multiple paths and multiple devices.  In the multiple-device case,
-    // the indices should match: if device 0 is a 32-bit GPU, then path 0
-    // should be bitcode for a GPU.  In the example below, we are loading
-    // bitcode for one device only.
+  // Instead of passing actual executable bits, we pass a path to the
+  // already-compiled bitcode to clCreateProgramWithBinary.  Note that
+  // you may load bitcode for multiple devices in one call by passing
+  // multiple paths and multiple devices.  In the multiple-device case,
+  // the indices should match: if device 0 is a 32-bit GPU, then path 0
+  // should be bitcode for a GPU.  In the example below, we are loading
+  // bitcode for one device only.
 
-    size_t len = strlen(bitcode_path);
-    program = clCreateProgramWithBinary(context, 1, &device, &len,
-                                        (const unsigned char**)&bitcode_path, NULL, &err);
-    check_status("clCreateProgramWithBinary", err);
+  size_t len = strlen(bitcode_path);
+  program = clCreateProgramWithBinary(context, 1, &device, &len,
+                                      (const unsigned char**)&bitcode_path, NULL, &err);
+  check_status("clCreateProgramWithBinary", err);
 
-    // The above tells OpenCL how to locate the intermediate bitcode, but we
-    // still must build the program to produce executable bits for our
-    // *specific* device.  This transforms gpu32 bitcode into actual executable
-    // bits for an AMD or Intel compute device (for example).
+  // The above tells OpenCL how to locate the intermediate bitcode, but we
+  // still must build the program to produce executable bits for our
+  // *specific* device.  This transforms gpu32 bitcode into actual executable
+  // bits for an AMD or Intel compute device (for example).
 
-    err = clBuildProgram(program, 1, &device, NULL, NULL, NULL);
-    check_status("clBuildProgram", err);
+  err = clBuildProgram(program, 1, &device, NULL, NULL, NULL);
+  check_status("clBuildProgram", err);
 
-    // And that's it -- we have a fully-compiled program created from the
-    // bitcode.  Let's ask OpenCL for the test kernel.
+  // And that's it -- we have a fully-compiled program created from the
+  // bitcode.  Let's ask OpenCL for the test kernel.
 
-    kernel = clCreateKernel(program, function_name, &err);
-    check_status("clCreateKernel", err);
-    
-    kernel_initialied = true;
+  kernel = clCreateKernel(program, function_name, &err);
+  check_status("clCreateKernel", err);
+
+  kernel_initialied = true;
 }
 
 static void create_program_from_bitcode(char* bitcode_path,
@@ -196,70 +196,72 @@ static void create_program_from_bitcode(char* bitcode_path,
                                         size_t programOutputSize,
                                         size_t count)
 {
-    cl_int err;
+  cl_int err;
 
-    init_kernel(bitcode_path,function_name);
-    // And now, let's test the kernel with some dummy data.
+  init_kernel(bitcode_path,function_name);
+  // And now, let's test the kernel with some dummy data.
 
-    // And create and load some CL memory buffers with that host data.
-    const int MAX_BUFFERS = 10;
-    cl_mem input_buffers[MAX_BUFFERS];
+  // And create and load some CL memory buffers with that host data.
+  const int MAX_BUFFERS = 10;
+  cl_mem input_buffers[MAX_BUFFERS];
 
-    int num_buffers = 0;
-    while(programInputs[num_buffers] != NULL)
+  int num_buffers = 0;
+  while(programInputs[num_buffers] != NULL)
+  {
+    input_buffers[num_buffers] = clCreateBuffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR,
+                                                programInputSizes[num_buffers], programInputs[num_buffers], &err);
+    if (input_buffers[num_buffers] == NULL)
     {
-        input_buffers[num_buffers] = clCreateBuffer(context, CL_MEM_READ_ONLY | CL_MEM_USE_HOST_PTR,
-                                                    programInputSizes[num_buffers], programInputs[num_buffers], &err);
-        if (input_buffers[num_buffers] == NULL)
-        {
-            fprintf(stderr, "Error: Unable to create OpenCL buffer memory objects %d.\n", num_buffers);
-            fprintf(stderr, "Using input %p of size %ld \n", programInputs[num_buffers], programInputSizes[num_buffers]);
-            exit(1);
-        }
-        ++num_buffers;
-
+      fprintf(stderr, "Error: Unable to create OpenCL buffer memory objects %d.\n", num_buffers);
+      fprintf(stderr, "Using input %p of size %ld \n", programInputs[num_buffers], programInputSizes[num_buffers]);
+      exit(1);
     }
+    ++num_buffers;
 
-    size_t total_sizes = 0;
-    for (int i = 0; i < num_buffers; ++i)
-    {
-        total_sizes += programInputSizes[i];
-    }
-    printf("total input_buffer_sizes: %ld\n", total_sizes);
+  }
 
-    // CL buffer 'c' is for output, so we don't prepopulate it with data.
+  size_t total_sizes = 0;
+  for (int i = 0; i < num_buffers; ++i)
+  {
+    total_sizes += programInputSizes[i];
+  }
+  printf("total input_sizes: %ld\n", total_sizes);
 
-    cl_mem outputBuffer = clCreateBuffer(context, CL_MEM_WRITE_ONLY,
-                                         programOutputSize, NULL, &err);
+  // CL buffer 'c' is for output, so we don't prepopulate it with data.
 
-    if (outputBuffer == NULL) {
-        fprintf(stderr, "Error: Unable to create OpenCL buffer memory objects.\n");
-        exit(1);
-    }
+  cl_mem outputBuffer = clCreateBuffer(context, CL_MEM_WRITE_ONLY,
+                                       programOutputSize, NULL, &err);
 
-    int argc = 0;
-    for (int i = 0; i < num_buffers; ++i){
-        err |= clSetKernelArg(kernel, argc++, sizeof(cl_mem), &input_buffers[i]);
-    }
-    err |= clSetKernelArg(kernel, argc++, sizeof(cl_mem), &outputBuffer);
-    err |= clSetKernelArg(kernel, argc++, sizeof(size_t), &count);
-    check_status("clSetKernelArg", err);
+  if (outputBuffer == NULL) {
+    fprintf(stderr, "Error: Unable to create OpenCL buffer memory objects.\n");
+    exit(1);
+  }
 
-    // Launch the kernel over a single dimension, which is the same size
-    // as the number of float4s.  We let OpenCL select the local dimensions
-    // by passing 'NULL' as the 6th parameter.
+  int argc = 0;
+  for (int i = 0; i < num_buffers; ++i){
+    err |= clSetKernelArg(kernel, argc++, sizeof(cl_mem), &input_buffers[i]);
+  }
+  err |= clSetKernelArg(kernel, argc++, sizeof(cl_mem), &outputBuffer);
+  err |= clSetKernelArg(kernel, argc++, sizeof(size_t), &count);
+  check_status("clSetKernelArg", err);
 
-    size_t global = count;
-    err = clEnqueueNDRangeKernel(queue, kernel, 1, NULL, &global, NULL, 0, NULL,
-                                 NULL);
-    check_status("clEnqueueNDRangeKernel", err);
+  // Launch the kernel over a single dimension, which is the same size
+  // as the number of float4s.  We let OpenCL select the local dimensions
+  // by passing 'NULL' as the 6th parameter.
 
-    clFinish(queue);
-    // Read back the results (blocking, so everything finishes), and then
-    // validate the results.
+  size_t global = count;
 
-    clEnqueueReadBuffer(queue, outputBuffer, CL_TRUE, 0, programOutputSize, programOutput,
-                        0, NULL, NULL);
+  fprintf(stderr, "About to clEnqueueNDRangeKernel.\n");
+  err = clEnqueueNDRangeKernel(queue, kernel, 1, NULL, &global, NULL, 0, NULL,
+                               NULL);
+  check_status("clEnqueueNDRangeKernel", err);
+
+  clFinish(queue);
+  // Read back the results (blocking, so everything finishes), and then
+  // validate the results.
+
+  clEnqueueReadBuffer(queue, outputBuffer, CL_TRUE, 0, programOutputSize, programOutput,
+                      0, NULL, NULL);
 }
 
 #pragma mark -
